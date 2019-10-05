dc31e673b24f6758292bd055cf9ba36b9a191eee
Rasan Rasch <rasan@nyu.edu>
2019-10-04 20:15:54 -0400

    Link to non-system libx264

--- doc/build-ffmpeg.sh
+++ doc/build-ffmpeg.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 #
-# Script to compile FFmpeg 2.1.6
+# Script to compile FFmpeg 2.1
 #
 # Author: Rasan Rasch <rasan@nyu.edu>
 
+X264_HOME="/usr/local/x264-0.152.2854+gite9a5903"
 FFMPEG_HOME="/usr/local/ffmpeg-2.1.6"
+
+export CFLAGS="-I$X264_HOME/include"
+export CXXFLAGS="-I$X264_HOME/include"
+export LDFLAGS="-L$X264_HOME/lib -Wl,-rpath,$X264_HOME/lib,-rpath,$FFMPEG_HOME/lib"
 
 sudo mkdir -p $FFMPEG_HOME/lib
 
 make clean
 
+# Ubuntu 18.04: disable opencv, schroedinger, openjpeg, and docs
+# https://ffmpeg.org/pipermail/ffmpeg-devel/2017-March/209128.html
+
 ./configure \
-    --prefix=/usr/local/ffmpeg-2.1.6 \
+    --prefix=$FFMPEG_HOME \
     --arch=x86_64 \
     --optflags="-O2 -g" \
     --enable-runtime-cpudetect \
@@ -28,18 +36,17 @@ make clean
     --enable-libgsm \
     --enable-libmp3lame \
     --enable-openal \
-    --enable-libopencv \
-    --enable-libopenjpeg \
+    --disable-libopencv \
+    --disable-libopenjpeg \
     --enable-libopus \
     --enable-libpulse \
     --enable-librtmp \
-    --enable-libschroedinger \
+    --disable-libschroedinger \
     --enable-libsoxr \
     --enable-libspeex \
     --enable-libtheora \
     --enable-libvorbis \
     --enable-libv4l2 \
-    --enable-libvpx \
     --enable-libx264 \
     --enable-libxvid \
     --enable-x11grab \
@@ -52,7 +59,7 @@ make clean
     --enable-gpl \
     --disable-debug \
     --disable-stripping \
-    --extra-ldflags="-Wl,-rpath $FFMPEG_HOME/lib"
+    --disable-doc
 
 make
 
